70b51cd2f1395328022f8f0951571ec6c8acc8a2
Linus-Daniel <vandulinus@gmail.com>
2023-07-30 02:59:38 +0100

    screen size

--- src/payment/LocalBank.jsx
+++ src/payment/LocalBank.jsx
@@ -4,11 +4,11 @@ import { Banks } from '../components/constants'
 import InfoCard from './components/InfoCard'
 let loader = null
 function LocalBank() {
-    useEffect(()=>{
-       const loader = setInterval(() => {
+    useEffect(() => {
+        const loader = setInterval(() => {
             console.log("me love my self")
         }, 5000);
-        return(
+        return (
             clearTimeout(loader)
         )
     })
@@ -16,69 +16,72 @@ function LocalBank() {
     const [product, setProduct] = useContext(ProductContext)
     console.log(bank)
     return (
-        <div className='mx-5 mt-10'>
+        <div className='mx-5 mt-10 h-screen  overflow-y-scroll mb-28'>
+            <div  className=' overflow-y-scroll mb-28 '>
 
-            <section>
-                <select  className='px-2 py-3 rounded-md mb-8' onChange={(event) => { setBank(event.target.value) }}>
-                {
-                    Banks.map((bank)=>{
-                        return(
 
-                    <option key={bank.name}>
-                        {bank.name}
-                    </option>
+                <section>
+                    <select className='px-2 py-3 rounded-md mb-8' onChange={(event) => { setBank(event.target.value) }}>
+                        {
+                            Banks.map((bank) => {
+                                return (
 
-)
-})
-}
-</select>
-            </section>
-            <div>
-                <p>Payment For </p>
-                <div className='flex px-2 my-3  text-sm justify-between '>
-                    <div>
-                        <img src={product.image} className='h-16 w-16' />
-                        <p>{product.name}</p>
-                        <p>{product.price}</p>
+                                    <option key={bank.name}>
+                                        {bank.name}
+                                    </option>
 
-                    </div>
-                    <div>
+                                )
+                            })
+                        }
+                    </select>
+                </section>
+                <div>
+                    <p>Payment For </p>
+                    <div className='flex px-2 my-3  text-sm justify-between '>
+                        <div>
+                            <img src={product.image} className='h-16 w-16' />
+                            <p>{product.name}</p>
+                            <p>{product.price}</p>
 
-                        <p>
-                            close Deliver According to location:
+                        </div>
+                        <div>
 
-                        </p>
-                        <p className='text-red-500'>
+                            <p>
+                                close Deliver According to location:
 
-                            Wunti Market Bauchi state
-                        </p>
-                        <button type='button' className='border-[1px] text-green-400 border-solid shadow-md px-1 py-2'>
-                            <select>
-                                <option>Change Delivery Location</option>
-                                <option>
-                                    Tudun wada jos
-                                </option>
-                                <option>
-                                    Rayfiend Jos
-                                </option>
-                                <option>
-                                    Rafin zurfi Bauchi
-                                </option>
-                                <option>
-                                    Zaramagand jos
-                                </option>
-                            </select>
-                        </button>
+                            </p>
+                            <p className='text-red-500'>
+
+                                Wunti Market Bauchi state
+                            </p>
+                            <button type='button' className='border-[1px] text-green-400 border-solid shadow-md px-1 py-2'>
+                                <select>
+                                    <option>Change Delivery Location</option>
+                                    <option>
+                                        Tudun wada jos
+                                    </option>
+                                    <option>
+                                        Rayfiend Jos
+                                    </option>
+                                    <option>
+                                        Rafin zurfi Bauchi
+                                    </option>
+                                    <option>
+                                        Zaramagand jos
+                                    </option>
+                                </select>
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-                <div className={`$`}>
+                <div className={`mb-10`}>
                     {
-                        Banks.map(banks=><InfoCard key={banks.name} bankInfo={banks} chosenBank={bank} />)
+                        Banks.map(banks => <InfoCard key={banks.name} bankInfo={banks} chosenBank={bank} />)
                     }
-            </div>
-            <button type='submit' onClick={(e)=>{if(confirm("Do you want to proceed?")===true){console.log("hi")}else{e.preventDefault()};}} className='text-lg rounded-md bg-black text-white mb-10 w-full py-3'>I have made Transaction</button>
+                </div>
+                <button type='submit' onClick={(e) => { if (confirm("Do you want to proceed?") === true) { console.log("hi") } else { e.preventDefault() }; }} className='text-lg rounded-md bg-black mt-5 text-white mb-28 w-full py-3'>I have made Transaction</button>
 
+            </div>
         </div>
     )
 }
